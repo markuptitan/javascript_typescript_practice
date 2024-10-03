8b5373ea906f383bbce2e636d28fcbc1c67c4fc3
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-10-03 12:40:25 +0200

    Second print statement

--- week-1/01-introduction-to-js.js
+++ week-1/01-introduction-to-js.js
@@ -1,2 +1,6 @@
 // Printing out text to the console
 console.log("Hi, my name is Samon");
+
+// Creating a variable and assigning a string value
+let name = "markuptitan";
+console.log(name + " is my username on all socials");
